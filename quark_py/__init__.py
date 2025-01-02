f44656c460fd7c7fcbc769f919fc5f344c87751f
QuarkA-V <nguyenvutrucnhan07122021@gmail.com>
2025-01-02 13:10:57 +0700

    Delete quark_py directory

--- quark_py/__init__.py
+++
@@ -1,5 +0,0 @@
-from .optimizer import QuantumOptimizer
-from .circuit import QuantumCircuit
-from .encoding import QubitEncoding
-from .consensus import ConsensusOptimizer
-from .compression import ModelCompressor
